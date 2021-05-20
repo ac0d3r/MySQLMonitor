f53303dbd4489c44f1d1425cc778fdeba4e6810e
Buzz2d0 <admin@imipy.com>
2021-05-20 13:04:54 +0800

    fixs

--- MySQLMonitor/MySQLMonitor.go
+++ MySQLMonitor/MySQLMonitor.go
@@ -6,161 +6,188 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"os/signal"
 	"strings"
-	"syscall"
 	"time"
 
-	"github.com/Buzz2d0/SecTools/pkg"
-	"github.com/Buzz2d0/SecTools/pkg/parse"
 	_ "github.com/go-sql-driver/mysql"
 )
 
-type logOutPutType string
-
-type logOutPut struct {
-	VariableName string `sql:"Variable_name"`
-	Value        string `sql:"Value"`
-}
-type execLog struct {
-	EventTime string `sql:"event_time"`
-	UserHost  string `sql:"user_host"`
-	Argument  string `sql:"argument"`
-}
-
-const (
-	fileLogOutPut  logOutPutType = "FILE"
-	tableLogOutPut logOutPutType = "TABLE"
-	// sql
-	setLogSQL            = "SET GLOBAL general_log='ON'"
-	showLogOutPutTypeSQL = "SHOW VARIABLES LIKE 'log_output'"
-	setLogOutPutTypeSQL  = "SET GLOBAL log_output=?"
-	getExecLogSQL        = `SELECT event_time, user_host, argument FROM mysql.general_log WHERE command_type ="Query" OR command_type ="Execute" ORDER BY event_time DESC LIMIT ?`
-)
-
 var (
-	sigs <-chan os.Signal
-
-	db         *sql.DB
-	limitNum   int = 1
-	lastone    time.Time
-	getExecLog = getExecLogSQL[:len(getExecLogSQL)-1]
-
 	flagHelp   = flag.Bool("h", false, "Shows usage options.")
 	flagHost   = flag.String("host", "localhost", "Bind mysql host.")
 	flagPort   = flag.Uint("port", 3306, "Bind mysql port.")
 	flagUser   = flag.String("user", "", "Select mysql username.")
 	flagPasswd = flag.String("passwd", "", "Input mysql password.")
+
+	flagStart = flag.Bool("start", false, "start mysql monitor.")
+	flagClean = flag.Bool("clean", false, "clean mysql general_log.")
+)
+
+var (
+	db *sql.DB
 )
 
 func banner() {
-	fmt.Println(`Start monitoring MySQL execution log...`)
-}
-
-func initDB() {
-	var err error
-	db, err = sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%d)/", *flagUser, *flagPasswd, *flagHost, *flagPort))
-	if err != nil {
-		log.Fatalf("connect to mysql failed: %q", err)
-	}
-	db.SetMaxOpenConns(20)
-	db.SetMaxIdleConns(10)
-}
-
-func checkMySQLLogOutPut() {
-	var (
-		logout logOutPut
-		row    *sql.Row
-		err    error
-	)
-	row = db.QueryRow(showLogOutPutTypeSQL)
-	err = row.Scan(&logout.VariableName, &logout.Value)
-	if err != nil {
-		log.Fatalf("exec %s failed: %q", showLogOutPutTypeSQL, err)
-	}
-	if logout.Value != string(tableLogOutPut) {
-		_, err = db.Exec(setLogOutPutTypeSQL, tableLogOutPut)
-		if err != nil {
-			log.Fatalf("exec %s failed: %q", setLogOutPutTypeSQL, err)
-		}
-	}
-}
-
-func printMySQLExecLog() bool {
-	var (
-		hasnew    bool
-		eventTime time.Time
-		rows      *sql.Rows
-		err       error
-	)
-
-	rows, err = db.Query(getExecLogSQL, limitNum)
-	if err != nil {
-		log.Fatalf("exec %s failed: %q", getExecLogSQL, err)
-	}
-	for rows.Next() {
-		var elog execLog
-		err = rows.Scan(&elog.EventTime, &elog.UserHost, &elog.Argument)
-		if err != nil {
-			log.Fatalf("printExecLog rows.Scan failed: %q", err)
-		}
-		eventTime, err = parse.Str2Time(elog.EventTime, "2006-01-02 15:04:05.000000")
-		if err != nil {
-			log.Fatalf("printExecLog time.format %s error: %q", elog.EventTime, err)
-			continue
-		}
-		// 初始化 lastone
-		if limitNum == 1 {
-			lastone = eventTime
-			limitNum = 5
-		} else if eventTime.After(lastone) {
-			if !strings.Contains(elog.Argument, getExecLog) {
-				hasnew = true
-				lastone = eventTime
-				fmt.Printf("[%s] - exec: %s\n", eventTime.Format("01-02 15:04:05"), elog.Argument)
-			}
-		}
-	}
-	return hasnew
+	t := `
+	███╗   ███╗██╗   ██╗███████╗ ██████╗ ██╗     
+	████╗ ████║╚██╗ ██╔╝██╔════╝██╔═══██╗██║     
+	██╔████╔██║ ╚████╔╝ ███████╗██║   ██║██║     
+	██║╚██╔╝██║  ╚██╔╝  ╚════██║██║▄▄ ██║██║     
+	██║ ╚═╝ ██║   ██║   ███████║╚██████╔╝███████╗
+	╚═╝     ╚═╝   ╚═╝   ╚══════╝ ╚══▀▀═╝ ╚══════╝
+███╗   ███╗ ██████╗ ███╗   ██╗██╗████████╗ ██████╗ ██████╗ 
+████╗ ████║██╔═══██╗████╗  ██║██║╚══██╔══╝██╔═══██╗██╔══██╗
+██╔████╔██║██║   ██║██╔██╗ ██║██║   ██║   ██║   ██║██████╔╝
+██║╚██╔╝██║██║   ██║██║╚██╗██║██║   ██║   ██║   ██║██╔══██╗
+██║ ╚═╝ ██║╚██████╔╝██║ ╚████║██║   ██║   ╚██████╔╝██║  ██║
+╚═╝     ╚═╝ ╚═════╝ ╚═╝  ╚═══╝╚═╝   ╚═╝    ╚═════╝ ╚═╝  ╚═╝`
+	fmt.Println(t)
 }
 
 func main() {
-	var (
-		hasnew bool
-		err    error
-	)
 	flag.Parse()
 	if *flagHelp || *flagUser == "" {
-		fmt.Printf("Usage: MySQLMonitor [options]\n\n")
+		fmt.Println("Usage: MySQLMonitor [options]")
 		flag.PrintDefaults()
 		return
 	}
 	banner()
-	initDB()
-
-	sigs = pkg.RegisterSignal(syscall.SIGINT, syscall.SIGTERM)
+	if err := initDB(); err != nil {
+		log.Fatalf("initDB error: %s", err)
+	}
 	defer func() {
+		if err := db.Close(); err != nil {
+			log.Printf("close database connection error: %s \n", err)
+		}
 		fmt.Println("\nBye hacker :)")
-		db.Close()
 	}()
 
-	_, err = db.Exec(setLogSQL)
-	if err != nil {
-		log.Fatalf("exec %s failed: %q", setLogSQL, err)
+	if *flagClean {
+		fmt.Println("clean mysql `general_log`...")
+		if err := cleanLog(); err != nil {
+			log.Fatalf("cleanLog error: %s", err)
+		}
+		return
 	}
-	checkMySQLLogOutPut()
 
+	if *flagStart {
+		fmt.Println("start mysql monitor ...")
+		if err := setMySQLLogOutput(); err != nil {
+			log.Fatalf("setMySQLLogOutput error: %s", err)
+		}
+		watchdog()
+	}
+}
+
+func initDB() error {
+	var err error
+	db, err = sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%d)/?parseTime=true", *flagUser, *flagPasswd, *flagHost, *flagPort))
+	if err != nil {
+		return err
+	}
+
+	db.SetMaxOpenConns(20)
+	db.SetMaxIdleConns(10)
+	return nil
+}
+
+type mysqlVariable struct {
+	Name  string `sql:"Variable_name"`
+	Value string `sql:"Value"`
+}
+
+func setMySQLLogOutput() error {
+	if _, err := db.Exec("SET GLOBAL general_log='ON'"); err != nil {
+		return err
+	}
+
+	variable := mysqlVariable{}
+	row := db.QueryRow("SHOW VARIABLES LIKE 'log_output'")
+	if err := row.Scan(&variable.Name, &variable.Value); err != nil {
+		return err
+	}
+	if variable.Name != "log_output" {
+		return fmt.Errorf("exec: `SHOW VARIABLES LIKE 'log_output'` not found log_output")
+	}
+	if variable.Value != "TABLE" {
+		if _, err := db.Exec("SET GLOBAL log_output = ? ", "TABLE"); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func cleanLog() error {
+	if _, err := db.Exec("RENAME TABLE mysql.`general_log` TO mysql.`general_log_temp`"); err != nil {
+		return err
+	}
+	if _, err := db.Exec("DELETE FROM mysql.`general_log_temp`"); err != nil {
+		return err
+	}
+	if _, err := db.Exec("RENAME TABLE mysql.`general_log_temp` TO mysql.`general_log`"); err != nil {
+		return err
+	}
+	return nil
+}
+
+type execLog struct {
+	EventTime time.Time `sql:"event_time"`
+	UserHost  string    `sql:"user_host"`
+	Argument  string    `sql:"argument"`
+}
+
+func watchdog() {
+	var (
+		hasnew  bool
+		limit   int = 1
+		lastone time.Time
+
+		execLogSQL = `SELECT event_time, user_host, argument FROM mysql.general_log WHERE command_type ="Query" OR command_type ="Execute" ORDER BY event_time DESC LIMIT ?`
+		sqlPrefix  = execLogSQL[:len(execLogSQL)-1]
+	)
+
+	defer func() {
+		if _, err := db.Exec("SET GLOBAL general_log='OFF'"); err != nil {
+			log.Printf("set `general_log`='OFF' error: %s \n", err)
+		}
+	}()
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+
+LOOP:
 	for {
 		select {
-		case <-sigs:
-			goto BREAK
+		case <-quit:
+			break LOOP
 		default:
-			hasnew = printMySQLExecLog()
+			// print execlog with table
+			hasnew = false
+			rows, err := db.Query(execLogSQL, limit)
+			if err != nil {
+				log.Printf("watchdog Query error: %s", err)
+				goto LOOP
+			}
+			for rows.Next() {
+				var elog execLog
+				err = rows.Scan(&elog.EventTime, &elog.UserHost, &elog.Argument)
+				if err != nil {
+					log.Printf("printExecLog rows.Scan error: %s", err)
+					continue
+				}
+				// 初始化 lastone
+				if limit == 1 {
+					limit = 5
+					lastone = elog.EventTime
+				} else if elog.EventTime.After(lastone) && !strings.Contains(elog.Argument, sqlPrefix) {
+					hasnew = true
+					lastone = elog.EventTime
+					fmt.Printf("[%s] - exec: %s\n", elog.EventTime.Format("2006-01-02 15:04:05"), elog.Argument)
+				}
+			}
 			if !hasnew {
 				time.Sleep(time.Millisecond * 150)
 			}
 		}
-		continue
-	BREAK:
-		break
 	}
 }
--- go.mod
+++ go.mod
@@ -5,7 +5,6 @@ go 1.15
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/go-sql-driver/mysql v1.5.0
-	github.com/google/gopacket v1.1.19
 	github.com/malfunkt/iprange v0.9.0
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/stretchr/testify v1.7.0 // indirect
