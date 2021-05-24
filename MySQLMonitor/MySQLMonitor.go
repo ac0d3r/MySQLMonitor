590bc20e1cb61a20cfd2e0ff984b910cb3b7bc6e
Buzz2d0 <admin@imipy.com>
2021-05-24 13:04:19 +0800

    MySQLMonitor 优化:)

--- MySQLMonitor/MySQLMonitor.go
+++ MySQLMonitor/MySQLMonitor.go
@@ -1,31 +1,34 @@
 package main
 
 import (
+	"bytes"
 	"database/sql"
 	"flag"
 	"fmt"
+	"io"
 	"log"
 	"os"
 	"os/signal"
 	"strings"
 	"time"
 
+	"github.com/Buzz2d0/SecTools/internal/parse"
+	"github.com/Buzz2d0/SecTools/internal/util"
 	_ "github.com/go-sql-driver/mysql"
 )
 
 var (
-	flagHelp   = flag.Bool("h", false, "Shows usage options.")
+	flagHelp   = flag.Bool("help", false, "Shows usage options.")
 	flagHost   = flag.String("host", "localhost", "Bind mysql host.")
 	flagPort   = flag.Uint("port", 3306, "Bind mysql port.")
-	flagUser   = flag.String("user", "", "Select mysql username.")
-	flagPasswd = flag.String("passwd", "", "Input mysql password.")
-
-	flagStart = flag.Bool("start", false, "start mysql monitor.")
-	flagClean = flag.Bool("clean", false, "clean mysql general_log.")
+	flagUser   = flag.String("u", "", "Select mysql username.")
+	flagPasswd = flag.String("p", "", "Input mysql password.")
 )
 
 var (
-	db *sql.DB
+	logfile string
+	db      *sql.DB
+	cstZone = time.FixedZone("CST", 8*3600)
 )
 
 func banner() {
@@ -52,37 +55,36 @@ func main() {
 		flag.PrintDefaults()
 		return
 	}
+	if !util.IsRoot() {
+		log.Fatalln("run as a user with root! Thx:)")
+	}
+
 	banner()
 	if err := initDB(); err != nil {
 		log.Fatalf("initDB error: %s", err)
 	}
+
 	defer func() {
+		if err := cleanGenerakLog(); err != nil {
+			log.Printf("cleanGenerakLog error: %s \n", err)
+		}
 		if err := db.Close(); err != nil {
 			log.Printf("close database connection error: %s \n", err)
 		}
 		fmt.Println("\nBye hacker :)")
 	}()
 
-	if *flagClean {
-		fmt.Println("clean mysql `general_log`...")
-		if err := cleanLog(); err != nil {
-			log.Fatalf("cleanLog error: %s", err)
-		}
-		return
+	fmt.Println("start mysql monitor ...")
+	if err := setMySQLLogOutput(); err != nil {
+		log.Fatalf("setMySQLLogOutput error: %s", err)
 	}
 
-	if *flagStart {
-		fmt.Println("start mysql monitor ...")
-		if err := setMySQLLogOutput(); err != nil {
-			log.Fatalf("setMySQLLogOutput error: %s", err)
-		}
-		watchdog()
-	}
+	watchdog()
 }
 
 func initDB() error {
 	var err error
-	db, err = sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%d)/?parseTime=true", *flagUser, *flagPasswd, *flagHost, *flagPort))
+	db, err = sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%d)/?charset=utf8mb4&parseTime=True&loc=Local", *flagUser, *flagPasswd, *flagHost, *flagPort))
 	if err != nil {
 		return err
 	}
@@ -98,96 +100,97 @@ type mysqlVariable struct {
 }
 
 func setMySQLLogOutput() error {
-	if _, err := db.Exec("SET GLOBAL general_log='ON'"); err != nil {
-		return err
-	}
-
 	variable := mysqlVariable{}
-	row := db.QueryRow("SHOW VARIABLES LIKE 'log_output'")
+	row := db.QueryRow("SHOW VARIABLES LIKE 'general_log_file'")
 	if err := row.Scan(&variable.Name, &variable.Value); err != nil {
 		return err
 	}
-	if variable.Name != "log_output" {
-		return fmt.Errorf("exec: `SHOW VARIABLES LIKE 'log_output'` not found log_output")
+	if variable.Name == "general_log_file" {
+		logfile = variable.Value
 	}
-	if variable.Value != "TABLE" {
-		if _, err := db.Exec("SET GLOBAL log_output = ? ", "TABLE"); err != nil {
-			return err
-		}
-	}
-	return nil
-}
 
-func cleanLog() error {
-	if _, err := db.Exec("RENAME TABLE mysql.`general_log` TO mysql.`general_log_temp`"); err != nil {
+	if _, err := db.Exec("SET GLOBAL log_output = 'FILE'"); err != nil {
 		return err
 	}
-	if _, err := db.Exec("DELETE FROM mysql.`general_log_temp`"); err != nil {
-		return err
-	}
-	if _, err := db.Exec("RENAME TABLE mysql.`general_log_temp` TO mysql.`general_log`"); err != nil {
+	if _, err := db.Exec("SET GLOBAL general_log='ON'"); err != nil {
 		return err
 	}
 	return nil
 }
 
-type execLog struct {
-	EventTime time.Time `sql:"event_time"`
-	UserHost  string    `sql:"user_host"`
-	Argument  string    `sql:"argument"`
+func cleanGenerakLog() error {
+	if _, err := db.Exec("SET GLOBAL general_log='OFF'"); err != nil {
+		return err
+	}
+	if logfile != "" {
+		return os.Truncate(logfile, 0)
+	}
+	return nil
 }
 
 func watchdog() {
-	var (
-		hasnew  bool
-		limit   int = 1
-		lastone time.Time
+	var f *os.File
 
-		execLogSQL = `SELECT event_time, user_host, argument FROM mysql.general_log WHERE command_type ="Query" OR command_type ="Execute" ORDER BY event_time DESC LIMIT ?`
-		sqlPrefix  = execLogSQL[:len(execLogSQL)-1]
-	)
+	if logfile == "" {
+		log.Fatalln("general_log_file was empty :(")
+	}
+	f, err := os.Open(logfile)
+	if err != nil {
+		log.Fatalf("Open '%s' error: %s", logfile, err)
+	}
+	defer f.Close()
+	// 指向文件尾部
+	_, err = f.Seek(0, 2)
+	if err != nil {
+		log.Fatalf("'%s' File.Seek(0,2) error: %s", logfile, err)
+	}
 
-	defer func() {
-		if _, err := db.Exec("SET GLOBAL general_log='OFF'"); err != nil {
-			log.Printf("set `general_log`='OFF' error: %s \n", err)
-		}
-	}()
 	quit := make(chan os.Signal, 1)
 	signal.Notify(quit, os.Interrupt)
-
 LOOP:
 	for {
 		select {
 		case <-quit:
 			break LOOP
 		default:
-			// print execlog with table
-			hasnew = false
-			rows, err := db.Query(execLogSQL, limit)
-			if err != nil {
-				log.Printf("watchdog Query error: %s", err)
-				goto LOOP
-			}
-			for rows.Next() {
-				var elog execLog
-				err = rows.Scan(&elog.EventTime, &elog.UserHost, &elog.Argument)
-				if err != nil {
-					log.Printf("printExecLog rows.Scan error: %s", err)
-					continue
-				}
-				// 初始化 lastone
-				if limit == 1 {
-					limit = 5
-					lastone = elog.EventTime
-				} else if elog.EventTime.After(lastone) && !strings.Contains(elog.Argument, sqlPrefix) {
-					hasnew = true
-					lastone = elog.EventTime
-					fmt.Printf("[%s] - exec: %s\n", elog.EventTime.Format("2006-01-02 15:04:05"), elog.Argument)
+			handle := func(line string) {
+				if strings.Contains(line, "Execute") || strings.Contains(line, "Query") {
+					lines := strings.Split(line, "\t")
+					t, err := parse.Str2Time(lines[0], "2006-01-02T15:04:05Z")
+					if err == nil {
+						fmt.Printf("[%s] %s\n", t.In(cstZone).Format("15:04:05"), lines[2])
+					} else {
+						fmt.Printf("[%s] %s\n", lines[0], lines[2])
+					}
 				}
 			}
-			if !hasnew {
-				time.Sleep(time.Millisecond * 150)
+			if err := linePrinter(f, handle); err != nil {
+				log.Printf("linePrinter error: %s \n", err)
+				break LOOP
 			}
+			time.Sleep(time.Millisecond * 550)
+		}
+	}
+}
+
+func linePrinter(r io.Reader, call func(string)) error {
+	buf := make([]byte, 32*1024)
+	lineSep := []byte{'\n'}
+
+	for {
+		c, err := r.Read(buf)
+		if c == 0 {
+			return nil
+		}
+		lines := bytes.Split(buf[:c], lineSep)
+		for i := range lines {
+			call(string(lines[i]))
+		}
+		switch {
+		case err == io.EOF:
+			return nil
+		case err != nil:
+			return err
 		}
 	}
 }
